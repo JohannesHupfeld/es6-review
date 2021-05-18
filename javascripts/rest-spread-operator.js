0c8e7f0b8992191013349782b628d61b87c8b703
Matteo <gnappo.1@live.it>
2021-05-18 13:37:08 -0700

    Pre lecture cleanup

--- javascripts/rest-spread-operator.js
+++ javascripts/rest-spread-operator.js
@@ -1,18 +1,1 @@
-function sayName(name, ...rest) {
-    console.log(`Hello ${name}! Here's the extra arguments list: ${rest}`)
-}
-
-const state = {
-    status: "active",
-    students: ["Matteo", "Cernan", "Laura"]
-}
-
-const newState = {
-    ...state,
-    status: "inactive"
-}
-
-function f(x, y, z) {
-    return x + y + z;
-  }
-console.log(f(4, ...[2,3]) == 9)
+// Rest, Spread Operator
